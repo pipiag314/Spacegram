9b9f0ea1f5058840bd2ba2fb87ad67ada2bbb259
gela pipia <gelapipia10@gmail.com>
2024-02-16 16:30:48 +0400

    Created color, screen variables, Installed tailwindcss-animate

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./global.css"
 
 const App = () => {
   return (
-    <div className="text-8xl text-red-400">Hello tailwindcss</div>
+    <div className="text-8xl underline">Hello tailwindcss</div>
   )
 }
 export default App
