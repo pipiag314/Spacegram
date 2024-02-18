e54373b03226f4e0db1f7de0ce15f358e1caae97
gela pipia <gelapipia10@gmail.com>
2024-02-18 18:41:47 +0400

    added routing & providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import "./global.css"
+import RootLayout from "./_root/RootLayout";
+import AuthLayout from "./_auth/AuthLayout";
+import SignIn from "./_auth/forms/SignIn";
+import SignUp from "./_auth/forms/SignUp";
+import { Home } from "./_root/pages";
+
+import { Toaster } from "./components/ui/toaster";
+
+import { Routes, Route} from "react-router-dom";
+
+import "./global.css";
+
 
 const App = () => {
   return (
-    <div className="text-8xl underline">Hello tailwindcss</div>
+    <main className="h-screen flex">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </main>
   )
-}
-export default App
+};
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { UserContextProvider } from "./context/UserContext.tsx";
+import { QueryProvider } from "./lib/query/QueryProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <QueryProvider>
+        <UserContextProvider>
+          <App />
+        </UserContextProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
