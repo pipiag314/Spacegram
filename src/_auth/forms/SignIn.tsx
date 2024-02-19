84956ecb8c460e6c06b131a9f218aea8707e3488
gela pipia <gelapipia10@gmail.com>
2024-02-19 16:20:47 +0400

    added sign-in form ui and validation

--- src/_auth/forms/SignIn.tsx
+++ src/_auth/forms/SignIn.tsx
@@ -1,10 +1,84 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { signInValidation } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const SignIn = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof signInValidation>>({
+    resolver: zodResolver(signInValidation),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof signInValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div>
-      <Button>Sign in</Button>
+      <Form {...form}>
+        <div className="flex justify-center font-bold text-3xl gap-2">
+          <img
+            width={30}
+            height={30}
+            className="object-contain"
+            src="/assets/icons/favicon.ico"
+          />
+          Spacegram
+        </div>
+        <h2 className="text-[24px] md:text-[30px] font-bold leading-[140%] tracking-tighter">
+          Sign In to your account
+        </h2>
+        <form
+          className="flex flex-col w-full gap-5 mt-4"
+          onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input className="input" type="text" placeholder="Enter your Username" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input className="input" type="password" placeholder="Enter your Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
-  )
-}
-export default SignIn
+  );
+};
+export default SignIn;
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -6,3 +6,9 @@ export const signUpValidation = z.object({
     email: z.string().email(),
     password: z.string().min(8, { message: "* Password must be longer than 8 character"})
 });
+
+ 
+export const signInValidation = z.object({
+  username: z.string().min(2, {message: "Username must be more than 2 character"}).max(50),
+  password: z.string().min(8, {message: "Password must be at least 8 character"}),
+})
