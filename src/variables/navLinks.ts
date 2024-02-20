29a8691696a5872efc5b1cbf19be00deec752721
gela pipia <gelapipia10@gmail.com>
2024-02-21 01:50:37 +0400

    created variables for navLinks

---
+++ src/variables/navLinks.ts
@@ -0,0 +1,32 @@
+export const sideBarLinks = [
+    {
+        label: "Home",
+        path: "/",
+        imageUrl: "assets/icons/home.svg",
+        alt: "home icon"
+    },
+    {
+        label: "Explore",
+        path: "/explore",
+        imageUrl: "assets/icons/posts.svg",
+        alt: "explore icon"
+    },
+    {
+        label: "People",
+        path: "/people",
+        imageUrl: "assets/icons/people.svg",
+        alt: "people icon"
+    },
+    {
+        label: "Saved",
+        path: "/saved",
+        imageUrl: "assets/icons/save.svg",
+        alt: "saved icon"
+    },
+    {
+        label: "Create Post",
+        path: "/create-post",
+        imageUrl: "assets/icons/gallery-add.svg",
+        alt: "create post icon"
+    },
+]
