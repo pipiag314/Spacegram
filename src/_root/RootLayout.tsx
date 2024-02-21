000ec7479ddb07fdc4d67755c57a0fed63f6dd7e
gela pipia <gelapipia10@gmail.com>
2024-02-21 21:31:46 +0400

    ADDED: TopBar and desktop navigation sidebar

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,14 +1,21 @@
+import BottomBar from "@/components/shared/BottomBar";
+import SideBar from "@/components/shared/SideBar";
+import TopBar from "@/components/shared/TopBar";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   
   return (
-    <>
-      <h1>Root Layout</h1>
-      <section>
+    <div className="w-full md:flex">
+      <TopBar />
+      <SideBar />
+
+      <section className="h-full">
         <Outlet />
       </section>
-    </>
+
+      <BottomBar />
+    </div>
   )
 }
 export default RootLayout;
