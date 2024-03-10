36e0de1d8ad3ba497ed275121df38b5505d8a2cc
gela pipia <gelapipia10@gmail.com>
2024-03-10 15:25:15 +0400

    dynamic style based on which page we are in BottomBar

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,21 +1,26 @@
-import { bottomBarLinks } from "@/variables/navLinks"
-import { Link } from "react-router-dom"
+import { bottomBarLinks } from "@/variables/navLinks";
+import { Link, useLocation } from "react-router-dom";
 
 const BottomBar = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className="bottombar">
       <nav>
         <ul className="flex justify-evenly">
-          {bottomBarLinks.map(link => (
-            <li key={link.imageUrl}>
-              <Link to={link.path}>
-                <img src={link.imageUrl} width={50} alt={link.alt} />
-              </Link>
-            </li>
-          ))}
+          {bottomBarLinks.map((link) => {
+            const isActiveLink = pathname === link.path;
+            return (
+              <li key={link.imageUrl} className={`sidebar-link group ${isActiveLink && "bg-primary-500"}`}>
+                <Link to={link.path}>
+                  <img src={link.imageUrl} className={`group-hover:brightness-0 ${isActiveLink && "brightness-0"}`} width={50} alt={link.alt} />
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </div>
-  )
-}
-export default BottomBar
+  );
+};
+export default BottomBar;
