91c86066fa6a25a4eee8db0fb9f0b87e4acf0d83
gela pipia <gelapipia10@gmail.com>
2024-03-11 00:22:43 +0400

    ADDED: validation for create post form

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -12,3 +12,16 @@ export const signInValidation = z.object({
   email: z.string().email({message: "* Invalid Email"}),
   password: z.string().min(8, {message: "* Password must be at least 8 character"}),
 })
+
+
+const MAX_FILE_SIZE = 1024 * 1024 * 5;
+
+export const createPostValidation = z.object({
+  caption: z.string().min(2, {
+    message: "Captions must be at least 2 characters.",
+  }),
+  file: z.instanceof(File).refine((file) => {
+    return !file || file.size <= MAX_FILE_SIZE
+  }, "File size must be less than 5MB"),
+  tags: z.string().optional()
+});
