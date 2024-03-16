615fc7221c191d471c67759dc1e208f9260d89cf
gela pipia <gelapipia10@gmail.com>
2024-03-16 20:40:19 +0400

    added functions for uploading image files to DB, getting fule URL, deleting FIle and creating new post

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,23 @@
 import { ID, Query } from "appwrite";
-import { NewUserType } from "@/types";
-import { account, appwriteConfig, avatars, databases } from "./config";
+import { NewUserType, PostType } from "@/types";
+import { account, appwriteConfig, avatars, databases, storage } from "./config";
+
+
+
+export const saveUserToDatabase = async (user: {
+    accountId: string;
+    name: string;
+    email: string;
+    imageUrl: URL;
+    username?: string;
+}) => {
+    try {
+        const newUser = await databases.createDocument(appwriteConfig.databaseId, appwriteConfig.usersCollectionId, ID.unique(), user)
+        return newUser;
+    } catch (error) {
+        console.log("Error while saveing user into DB: ", error);
+    }
+}
 
 export const createNewUser = async (user: NewUserType) => {
     try {
@@ -29,23 +46,6 @@ export const createNewUser = async (user: NewUserType) => {
         return error;
     }
 }
-
-
-export const saveUserToDatabase = async (user: {
-    accountId: string;
-    name: string;
-    email: string;
-    imageUrl: URL;
-    username?: string;
-}) => {
-    try {
-        const newUser = await databases.createDocument(appwriteConfig.databaseId, appwriteConfig.usersCollectionId, ID.unique(), user)
-        return newUser;
-    } catch (error) {
-        console.log("Error while saveing user into DB: ", error);
-    }
-}
-
 
 export const signInAccount = async (user: {email: string; password: string}) => {
     try {
@@ -83,5 +83,83 @@ export const signOutFromAccount = async () => {
     
     } catch (error) {
         console.log("Error while signing out account: ", error);
+    }
+}
+
+export const uploadFileToStorage = async (file: File) => {
+    try {
+        const res = await storage.createFile(appwriteConfig.storageId, ID.unique(), file)
+        return res;
+    } catch (error) {
+        console.log("Error while uploading file to storage: ", error)
+    }
+}
+
+export const getFileUrl = (fileId: string) => {
+    try {
+        const fileUrl = storage.getFilePreview(
+            appwriteConfig.storageId,
+            fileId,
+            2000,
+            2000,
+            "",
+            100
+        );
+        
+        if(!fileUrl) throw new Error("Error occured while getting file url")
+
+        return fileUrl;
+    } catch (error) {
+        console.log("Error occured while getting fileUrl: ", error);
+    }
+}
+
+export async function deleteFile(fileId: string) {
+    try {
+        await storage.deleteFile(appwriteConfig.storageId, fileId);
+
+        return { status: "ok" };
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const createNewPost = async (post: PostType) => {
+    try {
+        const uploaded = await uploadFileToStorage(post.file[0]);
+
+        if(!uploaded) throw new Error("Error while uploading file into storage");
+
+        const fileUrl = getFileUrl(uploaded.$id);
+
+        if(!fileUrl) {
+            deleteFile(uploaded.$id)
+            throw new Error("can't get fileUrl");
+        }
+
+        const tags = post.tags?.replace(/ /g, "").split(",") || [];
+
+        const newPost = await databases.createDocument(
+            appwriteConfig.databaseId, 
+            appwriteConfig.postsCollectionId, 
+            ID.unique(), 
+            {
+                creator: post.userId,
+                caption: post.caption,
+                imageUrl: fileUrl,
+                imageId: uploaded.$id,
+                location: post.location,
+                tags: tags,
+            }
+        );
+
+        if(!newPost) {
+            deleteFile(uploaded.$id)
+            throw new Error("Error while creating new post")
+        }
+        
+        return newPost;
+    } catch (error) {
+        console.log("Error while saving post into DB: ", error)
     }
 }
