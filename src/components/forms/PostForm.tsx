0ffe33aebcd8a9fed40cbeee972dac0ed9a4374e
gela pipia <gelapipia10@gmail.com>
2024-03-16 20:38:33 +0400

    added PostForm and FileUploader ui components

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -4,33 +4,57 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea"
 import { Input } from "../ui/input";
 import FileUploader from "../shared/FileUploader";
+import { ID, Models } from "appwrite";
+import { useCreatePost } from "@/lib/query/mutations";
+import { useUserContext } from "@/context/UserContext";
+import { useToast } from "../ui/use-toast";
+import { useNavigate } from "react-router-dom";
 
-const PostForm = () => {
+type PostFormProps = {
+  post?: Models.Document;
+}
+
+const PostForm = ({ post }: PostFormProps) => {
+
+  const { mutateAsync: createPost, isPending: isCreatingPost} = useCreatePost();
+  const { user } = useUserContext();
+  const { toast } = useToast();
+  
+  const navigate = useNavigate();
+  
   // 1. Define your form.
   const form = useForm<z.infer<typeof createPostValidation>>({
     resolver: zodResolver(createPostValidation),
     defaultValues: {
-      file: undefined,
+      file: [],
+      caption: post ? post?.caption : "",
+      location: post ? post?.location : "",
+      tags: post ? post?.tags.join(",") : "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof createPostValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof createPostValidation>) {
+    const newPost = await createPost({
+      ...values,
+      userId: user.id,
+    })
+
+    if(!newPost) {
+      toast({
+        title: "Something went wrong, try again later."
+      })
+    } else {
+      toast({
+        title: "Post uploaded succesfully."
+      })
+    }
+
+    navigate("/");
   }
 
   return (
@@ -56,8 +80,21 @@ const PostForm = () => {
             <FormItem>
               <FormLabel>Add Photo</FormLabel>
               <FormControl>
-                <FileUploader />
+                <FileUploader fieldChange={field.onChange} mediaUrl={post?.imageUrl} />
                 {/* <Input  placeholder="Caption" {...field} className="input custom-scrollbar" /> */}
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Add a location</FormLabel>
+              <FormControl>
+                <Input  placeholder="location" {...field} className="input" />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -70,7 +107,7 @@ const PostForm = () => {
             <FormItem>
               <FormLabel>Add Tags, separate them with comma (",")</FormLabel>
               <FormControl>
-                <Input  placeholder="Caption" {...field} className="input custom-scrollbar" />
+                <Input  placeholder="Caption" {...field} className="input" />
               </FormControl>
               <FormMessage />
             </FormItem>
--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,14 +1,20 @@
-import { useCallback, useState } from "react";
-import { useDropzone } from "react-dropzone";
-import { Button } from "../ui/button";
+import { useCallback, useEffect, useState } from "react";
+import { useDropzone, FileWithPath } from "react-dropzone";
 
-const FileUploader = () => {
-  const [file, setFile] = useState("");
+type FileUploaderProps = {
+  fieldChange: (Files: File[]) => void;
+  mediaUrl: string;
+}
+
+const FileUploader = ({ fieldChange, mediaUrl}: FileUploaderProps) => {
+  const [file, setFile] = useState<File[]>([]);
+  const [fileUrl, setFileUrl] = useState("");
 
   const onDrop = useCallback(
-    (acceptedFiles: any) => {
-      // console.log(acceptedFiles)
-      // setFile(acceptedFiles[0].path)
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles)
+      fieldChange(acceptedFiles)
+      setFileUrl(URL.createObjectURL(acceptedFiles[0]))
     },
     [file]
   );
@@ -23,8 +29,11 @@ const FileUploader = () => {
       {...getRootProps()}
       className="bg-dark-4 rounded-lg flex-center cursor-pointer p-5">
       <input {...getInputProps()} />
-      {file ? (
-        <div>{file}</div>
+      {fileUrl ? (
+        <div className="flex flex-col justify-center items-center gap-5">
+          <img src={fileUrl} alt="post image" className="h-80 lg:h-[480px] w-full rounded-[24px] object-cover object-top" />
+          <p className="text-light-3">Drag or Click here for replace with other photo</p>
+        </div>
       ) : (
         <div className="flex-center flex-col p-7 h-80">
           <img
