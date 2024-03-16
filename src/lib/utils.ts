f206e70aafbd45be4d5599b1fdc5e4d11e92fc7d
gela pipia <gelapipia10@gmail.com>
2024-03-16 23:47:58 +0400

    helper function for date formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function calculateTimeAgo(fullDate: string): string {
+  const currentDate = new Date();
+  const pastDate = new Date(fullDate);
+
+  const timeDifference = currentDate.getTime() - pastDate.getTime();
+  const millisecondsInOneHour = 1000 * 60 * 60;
+  const millisecondsInOneDay = millisecondsInOneHour * 24;
+
+  const daysAgo = Math.floor(timeDifference / millisecondsInOneDay);
+  const hoursAgo = Math.floor((timeDifference % millisecondsInOneDay) / millisecondsInOneHour);
+
+  if (daysAgo > 0) {
+      return `${daysAgo} day${daysAgo === 1 ? '' : 's'} ago`;
+  } else if (hoursAgo > 0) {
+      return `${hoursAgo} hour${hoursAgo === 1 ? '' : 's'} ago`;
+  } else {
+      return "Less than an hour ago";
+  }
+}
