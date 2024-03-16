3a1dd755c086f127bcbedd6433e1886d3c0d3db1
gela pipia <gelapipia10@gmail.com>
2024-03-17 00:43:34 +0400

    Home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import Loader from "@/components/shared/Loader";
+import PostCard from "@/components/shared/PostCard";
+import { useGetRecentPosts } from "@/lib/query/mutations";
+import { Models } from "appwrite";
 
 const Home = () => {
-  const [name, setName] = useState("");
+  const { data: posts, isError, isPending: isLoadingPosts } = useGetRecentPosts();
   
   return (
-    <div className="text-red">
-      <div>Welcome {name}</div>
-      <input type="text" onChange={(e) => setName(e.target.value)} />
+    <div className="flex flex-1">
+      <div className="main-container">
+        <div className="home_posts-container">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Feeds.</h2>
+          {isLoadingPosts && !posts ? (
+            <Loader />
+          ) : (
+            <ul className="flex flex-col flex-1 gap-5 lg:gap-8 w-full">
+              {posts?.documents.map((post: Models.Document) => (
+                <li key={post.$id}>
+                  <PostCard post={post} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
