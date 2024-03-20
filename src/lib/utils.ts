618c49a7beaeaf308885148ae667fa0dddd752e2
gela pipia <gelapipia10@gmail.com>
2024-03-20 16:47:16 +0400

    FIX: incorrect calculating time in calculateTimeAgo helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,24 +9,24 @@ export function cn(...inputs: ClassValue[]) {
 export function calculateTimeAgo(fullDate: string): string {
   const currentDate = new Date();
   const pastDate = new Date(fullDate);
-
   const timeDifference = currentDate.getTime() - pastDate.getTime();
-  const millisecondsInOneMinute = 1000 * 60;
-  const millisecondsInOneHour = millisecondsInOneMinute * 60;
-  const millisecondsInOneDay = millisecondsInOneHour * 24;
 
-  const minutesAgo = Math.floor(timeDifference / millisecondsInOneMinute);
-  const hoursAgo = Math.floor((timeDifference % millisecondsInOneDay) / millisecondsInOneHour);
-  const daysAgo = Math.floor((timeDifference % millisecondsInOneHour) / millisecondsInOneDay);
+  const millisecondsInMinute = 1000 * 60;
+  const millisecondsInHour = millisecondsInMinute * 60;
+  const millisecondsInDay = millisecondsInHour * 24;
+
+  const minutesAgo = Math.floor(timeDifference / millisecondsInMinute);
+  const hoursAgo = Math.floor((timeDifference % millisecondsInDay) / millisecondsInHour);
+  const daysAgo = Math.floor(timeDifference / millisecondsInDay);
 
   if (daysAgo > 0) {
-    return `${daysAgo} day${daysAgo === 1 ? '' : 's'} ago`;
+      return `${daysAgo} day${daysAgo === 1 ? '' : 's'} ago`;
   } else if (hoursAgo > 0) {
-    return `${hoursAgo} hour${hoursAgo === 1 ? '' : 's'} ago`;
+      return `${hoursAgo} hour${hoursAgo === 1 ? '' : 's'} ago`;
   } else if (minutesAgo > 0) {
-    return `${minutesAgo} minute${minutesAgo === 1 ? '' : 's'} ago`;
+      return `${minutesAgo} minute${minutesAgo === 1 ? '' : 's'} ago`;
   } else {
-    return "Just now";
+      return "Just now";
   }
 }
 
